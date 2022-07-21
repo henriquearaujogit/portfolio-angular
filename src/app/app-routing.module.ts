f934de5cfad8b08ac450cf54c14d186448c5bfbf
henrique araujo <henriquedotdev@gmail.com>
2022-07-20 21:38:57 -0300

    criação e diagramação basica do site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { TodoHomeComponent } from './todo-home/todo-home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'projetos', component: ProjetosComponent},
+  {path: 'sobre', component: SobreComponent},
+  {path: 'contato', component: SobreComponent},
+  {path: 'todo/:user_id', component: TodoHomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoHomeComponent } from './todo-home/todo-home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      TodoHomeComponent,
+      HeaderComponent,
+      FooterComponent,
+      HomeComponent,
+      ProjetosComponent,
+      SobreComponent,
+      ContatoComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
